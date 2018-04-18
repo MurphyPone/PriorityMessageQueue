0ebcf02582b2abd186fb68ec798f9069cbef7f9d
"[MurphyPOne]" <"[murphyp1@bishopireton.org]">
2018-04-17 20:44:19 -0400

    Added some debugging stuff

--- PriorityMessageQueue/src/PriorityMessageQueue.java
+++ PriorityMessageQueue/src/PriorityMessageQueue.java
@@ -61,18 +61,20 @@ public class PriorityMessageQueue {
 		}
 		if(m != null) 
 			return m;
-		else throw new NoSuchElementException();
+		else 
+			throw new NoSuchElementException();
 	}
 	
 	//Process 
 	public void process() {
-		while(!isEmpty() && time < 10000) {
-			if(time % 4 == 0)  //every 4 minutes
+		while( !isEmpty() ) { //TODO re-Add condition to stop after x minutes
+			if(time % 4 == 0) {   //every 4 minutes
 				output.add( remove() );	//Add the highest priority message to the Queue 		
+			}
 			
-			add(new Message(time) );	//Create a new message every minute //TODO 99% of the Messages are priority 4 even though it should be evenly distr...
+			//add(new Message(time) );	//Create a new message every minute //TODO 99% of the Messages are priority 4 even though it should be evenly distr...
 			time++;	//every iteration = 1 minute
-			//System.out.println(toString()); //TODO shows that there are in fact more than 0 messages in p0
+			//System.out.println(toString());
 		}
 		//After processing all, output analysis
 		System.out.print( analyze() );
@@ -83,7 +85,7 @@ public class PriorityMessageQueue {
 		double avg = 0;	//overall avg wait time
 		double[] avgs = new double[numQs]; //stores the avgs for each of the individual pqs
 		int[] msgCount = new int[numQs]; //Keeps track of the # of messages per Queue
-		int numMessages = output.size() - 1;
+		int numMessages = output.size();
 		
 		//sum wait times 
 		for(Message m : output) {
@@ -93,6 +95,7 @@ public class PriorityMessageQueue {
 			avgs[p] += m.getTimeOfArrival(); //fill first
 			msgCount[p]++;	//keep track of how many messages in each queue
 		}
+		
 		//divides by total messages
 		avg = (double) avg / numMessages;	//total avg
 		//Build String
@@ -107,6 +110,7 @@ public class PriorityMessageQueue {
 		return result;
 	}
 	
+	//toString 
 	public String toString() {
 		String result = "";
 		int i = 0;
@@ -119,12 +123,35 @@ public class PriorityMessageQueue {
 		//Create some test msgs
 		int nm = 100; //num messages
 		PriorityMessageQueue x = new PriorityMessageQueue(); //test PMQ
-		//TODO make sure that I process the same amount of messages for each pq
-		//int arr[] keep track
+		
+		//debugging tracker
+		int[] priorityTracker = new int[5];
+		int[] actual = new int[5];
+		//
  
+		//Add initial messages
 		for(int i = 0; i < nm; i++) {
-			x.add( new Message(0) );	//add with beginning time 0
+			int makeSureThatTheyHaveARandomPriority = (int) (Math.random() * 5);
+			priorityTracker[makeSureThatTheyHaveARandomPriority]++; //Debugging tracker
+			Message m = new Message(0); //add with beginning time 0
+			m.setPriority(makeSureThatTheyHaveARandomPriority);
+			x.add( m );	
 		}
+		
 		x.process();
+		
+		//Debugging tracker
+		for(Message m : x.output) {
+			int p = m.getPriority();
+			actual[p]++;
+		}
+		
+		for(int i = 0; i < 5; i++) {
+			String r = "\nProjected q" + i + ": " + priorityTracker[i];
+				r += "\nActual q" + i + ": " + actual[i];
+			System.out.println(r);
+		} 
+		//End debugging tracker --> pulling hair out
+			
 	}
 }
